77fd82088fa458b71caba2d0389c39140bcef2c0
tharsikan1999 <tharsikanking@gmail.com>
2024-05-17 13:35:16 +0530

    finish routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Welcome from "./components/Welcome";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import AddContact from "./components/AddContacts";
 import Contacts from "./components/Contacts";
+import AddContact from "./components/AddContacts";
 
-const App = () => {
-  return <Contacts />;
-};
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/contacts/new" element={<AddContact />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
