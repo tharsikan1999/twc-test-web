dbdfd8eab5a35e970ae87bbc5c125746d8719a74
tharsikan1999 <tharsikanking@gmail.com>
2024-05-17 17:00:39 +0530

    finish validation register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,8 +2,57 @@ import logo from "../assets/img/logo.png";
 import contactIMG from "../assets/img/contacts portal.png";
 import Ellipse from "../assets/img/auth-Ellipse.png";
 import { Link } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "react-toastify";
+
+const schema = z
+  .object({
+    email: z.string().email().min(1).max(255),
+    password: z
+      .string()
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .regex(/[0-9]/, {
+        message: "Password must contain at least one numeric digit",
+      })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: "Password must contain at least one special character",
+      })
+      .min(8),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+type FormFields = z.infer<typeof schema>;
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      toast.success("Registration successful");
+      console.log(data);
+    } catch (error) {
+      toast.error("Registration failed");
+      console.log(error);
+    }
+  };
+
   return (
     <main
       className="flex w-full items-center justify-between flex-col-reverse sm:flex-row sm:min-h-screen"
@@ -26,44 +75,54 @@ function Register() {
           </h1>
 
           {/* Registration Form */}
-          <form className="w-full mt-14">
+          <form className="w-full mt-14" onSubmit={handleSubmit(onSubmit)}>
             {/* Email Input */}
             <input
+              {...register("email")}
               type="email"
               name="email"
               className="bg-white rounded-3xl mb-10 focus:ring-blue-500 focus:border-blue-500 block w-full h-[55px] pl-[41px] dark:focus:ring-blue-500 dark:focus:border-blue-500 text-customGreen placeholder-customGreen md:w-[477px] lg:text-[25px] text-[20px] font-normal"
               placeholder="e-mail"
-              required
             />
+            {errors.email && (
+              <div className="text-red-500">{errors.email.message}</div>
+            )}
 
             {/* Password Input */}
             <input
+              {...register("password")}
               type="password"
               name="password"
               className="bg-white rounded-3xl mb-10 focus:ring-blue-500 focus:border-blue-500 block h-[55px] pl-[41px] dark:focus:ring-blue-500 dark:focus:border-blue-500 w-full text-customGreen placeholder-customGreen md:w-[477px] lg:text-[25px] text-[20px] font-normal"
               placeholder="create password"
-              required
             />
+            {errors.password && (
+              <div className="text-red-500">{errors.password.message}</div>
+            )}
 
             {/* Confirm Password Input */}
             <input
+              {...register("confirmPassword")}
               type="password"
               name="confirmPassword"
               className="bg-white rounded-3xl mb-10 focus:ring-blue-500 focus:border-blue-500 block w-full h-[55px] pl-[41px] dark:focus:ring-blue-500 dark:focus:border-blue-500 text-customGreen placeholder-customGreen md:w-[477px] lg:text-[25px] text-[20px] font-normal"
               placeholder="confirm password"
-              required
             />
+            {errors.confirmPassword && (
+              <div className="text-red-500">
+                {errors.confirmPassword.message}
+              </div>
+            )}
 
             {/* Register Button */}
             <div className="flex justify-center md:justify-start">
-              <Link to="/login">
-                <button
-                  type="submit"
-                  className="text-white bg-customGreen border-2 focus:outline-none focus:ring-gray-300 w-[149px]   h-[48px] md:text-[25px] text-[23px] border-1 border-white rounded-[50px] lg:mt-8 mt-4"
-                >
-                  register
-                </button>
-              </Link>
+              <button
+                disabled={isSubmitting}
+                type="submit"
+                className="text-white bg-customGreen border-2 focus:outline-none focus:ring-gray-300 w-[149px]   h-[48px] md:text-[25px] text-[23px] border-1 border-white rounded-[50px] lg:mt-8 mt-4"
+              >
+                {isSubmitting ? "Loading..." : "Register"}
+              </button>
             </div>
           </form>
 
--- src/components/learn.tsx
+++ src/components/learn.tsx
@@ -2,24 +2,30 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 
-const schema = z.object({
-  email: z.string().email().min(1).max(255),
-  password: z
-    .string()
-    .regex(/[A-Z]/, {
-      message: "Password must contain at least one uppercase letter",
-    })
-    .regex(/[a-z]/, {
-      message: "Password must contain at least one lowercase letter",
-    })
-    .regex(/[0-9]/, {
-      message: "Password must contain at least one numeric digit",
-    })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "Password must contain at least one special character",
-    })
-    .min(8),
-});
+const schema = z
+  .object({
+    email: z.string().email().min(1).max(255),
+    password: z
+      .string()
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .regex(/[0-9]/, {
+        message: "Password must contain at least one numeric digit",
+      })
+      .regex(/[^a-zA-Z0-9]/, {
+        message: "Password must contain at least one special character",
+      })
+      .min(8),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
 
 type FormFields = z.infer<typeof schema>;
 
@@ -28,6 +34,7 @@ const Learn = () => {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
+    watch,
   } = useForm<FormFields>({
     resolver: zodResolver(schema),
   });
@@ -40,6 +47,8 @@ const Learn = () => {
     }
   };
 
+  const password = watch("password");
+
   return (
     <form className="tutorial gap-2" onSubmit={handleSubmit(onSubmit)}>
       <input {...register("email")} type="text" placeholder="Email" />
@@ -49,6 +58,17 @@ const Learn = () => {
       <input {...register("password")} type="password" placeholder="Password" />
       {errors.password && (
         <div className="text-red-500">{errors.password.message}</div>
+      )}
+      <input
+        {...register("confirmPassword")}
+        type="password"
+        placeholder="Confirm Password"
+      />
+      {errors.confirmPassword && (
+        <div className="text-red-500">{errors.confirmPassword.message}</div>
+      )}
+      {password && (
+        <div className="text-red-500">{errors.confirmPassword?.message}</div>
       )}
       <button disabled={isSubmitting} type="submit">
         {isSubmitting ? "Loading..." : "Submit"}
