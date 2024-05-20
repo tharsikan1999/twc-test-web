4c4b0abf4488d53f26d6392cec7bab789e4d79f3
tharsikan1999 <tharsikanking@gmail.com>
2024-05-20 10:53:37 +0530

    finish delete contacts

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,7 @@
 // store.ts
 import create, { SetState } from "zustand";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 interface Contact {
   id: string;
@@ -29,10 +31,29 @@ const useStore = create<StoreState>((set: SetState<StoreState>) => ({
       ),
     })),
 
-  deleteContact: (id: string) =>
-    set((state) => ({
-      contacts: state.contacts.filter((contact) => contact.id !== id),
-    })),
+  deleteContact: async (id: string) => {
+    try {
+      const token = localStorage.getItem("jwt");
+
+      if (!token) {
+        throw new Error("JWT token not found in local storage");
+      }
+
+      await axios.delete(`http://localhost:3333/contacts/delete/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      set((state) => ({
+        contacts: state.contacts.filter((contact) => contact.id !== id),
+      }));
+      toast.success("Contact deleted successfully");
+    } catch (error) {
+      console.error("Error deleting contact:", error);
+      throw new Error("Failed to delete contact");
+    }
+  },
 }));
 
 export default useStore;
